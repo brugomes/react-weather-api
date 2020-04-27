535674988d05d9fac521cdb3da63b60abc56feac
Bruna Gomes da Silva <brugomes@users.noreply.github.com>
2020-04-27 21:38:04 +1200

    layout customization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Weather from './components/weather'
 
-import 'weather-icons/css/weather-icons.min.css';
+import './App.scss';
 
 function App() {
   return (
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './weather.scss'
+
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 function Weather() {
     const [location, setLocation] = useState(false);
@@ -15,9 +19,19 @@ function Weather() {
                 units: 'metric'
             }
         });
+
+        let data = {
+            'city': res.data.name,
+            'temp': Math.round(res.data.main.temp * 1) / 1,
+            'tempMin': Math.round(res.data.main.temp_min * 1) / 1,
+            'tempMax': Math.round(res.data.main.temp_max * 1) / 1,
+            'icon': res.data.weather[0].icon,
+            'description': res.data.weather[0].description,
+            'feels_like': Math.round(res.data.main.feels_like * 1) / 1,
+            'humidity': res.data.main.humidity
+        }
         
-        console.log(res.data)
-        setWeather(res.data);
+        setWeather(data);
     }
 
     useEffect(() => {
@@ -41,8 +55,27 @@ function Weather() {
         )
     } else {
         return (
-            <div className="App">
-                <h3>{weather['main']['temp']}°</h3>
+            <div className='weather-container'>
+                <h3>{weather['city']}</h3>
+
+                <div className='weather-container__weekdate-temp'>
+                    <span>
+                    <Moment format="dddd"></Moment>
+                    </span>
+                    <span>&#8595;{weather['tempMin']}° &nbsp;&#8593;{weather['tempMax']}°</span>
+                </div>
+
+                <div className='weather-container__temperature'>  
+                    <div><span>{weather['temp']}°</span></div>                  
+                    <div><img src={`http://openweathermap.org/img/wn/${weather['icon']}@2x.png`} alt=''/></div>
+                </div>
+
+                <p>{weather['description']}</p>
+
+                <div className='weather-container__information'>
+                    <span>feels like: {weather['feels_like']}°</span>
+                    <span>humidity: {weather['humidity']}%</span>
+                </div>
             </div>
         );
     }
